ea10442d21bb8180b833e7cba71fd355799b123e
naufal <naufal>
2025-05-31 00:28:53 +0700

    modified:   app/posts/[...filename]/page.tsx 	new file:   pages/demo/blog/[filename].tsx

--- app/posts/[...filename]/page.tsx
+++ app/posts/[...filename]/page.tsx
@@ -1,7 +1,14 @@
 import { client } from '../../../tina/__generated__/client'
 import Post from './client-page'
 
-export default async function Page({ params }: { params: { filename: string[] } }) {
+// Define a compatible type for params
+interface PageProps {
+  params: {
+    filename: string[];
+  };
+}
+
+export default async function Page({ params }: PageProps) {
   const filename = params.filename.join('/') + '.md'
   const { data, query, variables } = await client.queries.post({ relativePath: filename })
 
