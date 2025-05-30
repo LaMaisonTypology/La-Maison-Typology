a9ba8b49a5e38ce04f6009764bffbcb01ed292f7
naufal <naufal>
2025-05-31 00:58:00 +0700

    modified:   app/posts/[...filename]/page.tsx

--- app/posts/[...filename]/page.tsx
+++ app/posts/[...filename]/page.tsx
@@ -2,9 +2,8 @@ import { client } from '../../../tina/__generated__/client'
 import Post from './client-page'
 
 // Adjusted the type definition to align with Next.js expectations
-export default async function Page({ params }: { params: Promise<{ filename: string[] }> }) {
-  const resolvedParams = await params;
-  const filename = resolvedParams.filename.join('/') + '.md';
+export default async function Page({ params }: { params: { filename: string[] } }) {
+  const filename = params.filename.join('/') + '.md';
   const { data, query, variables } = await client.queries.post({ relativePath: filename });
 
   return <Post data={data} query={query} variables={variables} />;
