5bbe7de38f0fec2233a8051fe51db53e733c07d9
naufal <naufal>
2025-05-31 00:36:51 +0700

    modified:   app/posts/[...filename]/page.tsx

--- app/posts/[...filename]/page.tsx
+++ app/posts/[...filename]/page.tsx
@@ -1,17 +1,15 @@
 import { client } from '../../../tina/__generated__/client'
 import Post from './client-page'
-import { PageProps as NextPageProps } from 'next';
-import PageProps from "next"
 // Adjust the type definition to align with Next.js expectations
-interface PageProps extends NextPageProps {
+interface PageProps {
   params: {
     filename: string[];
   };
 }
 
-export default async function Page({ params }: PageProps) {
-  const filename = params.filename.join('/') + '.md'
-  const { data, query, variables } = await client.queries.post({ relativePath: filename })
+export default async function Page({ params }: { params: { filename: string[] } }) {
+  const filename = params.filename.join('/') + '.md';
+  const { data, query, variables } = await client.queries.post({ relativePath: filename });
 
-  return <Post data={data} query={query} variables={variables} />
+  return <Post data={data} query={query} variables={variables} />;
 }
